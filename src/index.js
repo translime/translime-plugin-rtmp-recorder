910302a4b2b8f5f7b5d4401f9067e8a77bb7b847
slime7 <slime7@foxmail.com>
2023-06-16 16:39:39 +0800

    save

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
+import fs from 'node:fs';
 import pkg from '../package.json';
+
+const ffmpeg = require('fluent-ffmpeg');
 
 const id = pkg.name;
 
+const setFfmpegPath = (ff, path) => {
+  try {
+    fs.accessSync(path);
+    ff.setFfmpegPath(path);
+  } catch (err) {
+    // mute
+  }
+};
 // 加载时执行
 const pluginDidLoad = () => {
-  console.log('plugin loaded');
   const setting = global.store.get(`plugin.${id}.settings`, {});
-  console.log('settings: ', setting);
+  if (setting['ffmpeg-path']) {
+    setFfmpegPath(ffmpeg, setting['ffmpeg-path']);
+  }
 };
 
 // 禁用时执行
@@ -14,58 +26,28 @@ const pluginWillUnload = () => {
   console.log('plugin unloaded');
 };
 
+const pluginSettingSaved = () => {
+  const setting = global.store.get(`plugin.${id}.settings`, {});
+  if (setting['ffmpeg-path']) {
+    setFfmpegPath(ffmpeg, setting['ffmpeg-path']);
+  }
+};
+
 // 插件设置表单
 const settingMenu = [
-  // 文本框
   {
-    key: 'input-1', // 设置储存到配置文件的字段，没有这个字段则取 name 的值
-    type: 'input', // 输入类型
-    name: '文本1', // 输入显示的字段名
-    required: false, // 是否必填
-    placeholder: '输入提示',
-  },
-  // 密码文本框
-  {
-    type: 'password',
-    name: '密码',
-    required: true,
-    placeholder: '请输入密码',
-  },
-  // 开关
-  {
-    type: 'switch',
-    name: '开关',
-  },
-  // 复选框
-  {
-    type: 'checkbox',
-    name: '复选',
-    choices: [
-      {
-        name: '选择1',
-        value: 'foo',
-      },
-      {
-        name: '选择2',
-        value: 'bar',
-      },
-      {
-        name: '选择3', // 没有 value 则用 name 作为值
-      },
-    ],
-  },
-  // 单选框
-  {
-    type: 'radio',
-    name: '单选',
-    choices: ['foo', 'bar'], // 可以使用复选框的方式，也可直接用文本数组，默认选择第一个
-  },
-  // 下拉菜单
-  {
-    type: 'list',
-    name: '下拉菜单',
-    required: true,
-    choices: ['foo', 'bar'],
+    key: 'ffmpeg-path',
+    type: 'file',
+    name: 'ffmpeg 路径',
+    required: false,
+    placeholder: '不设置则会查找系统环境变量中的 ffmpeg，没有找到则无法运行',
+    dialogOptions: {
+      filters: [
+        { name: 'ffmpeg', extensions: ['exe'] },
+        { name: '所有文件', extensions: ['*'] },
+      ],
+      properties: ['openFile', 'dontAddToRecent'],
+    },
   },
 ];
 
@@ -81,13 +63,38 @@ const pluginMenu = [
   },
 ];
 
+let currentProgress = {};
 // ipc 定义
 const ipcHandlers = [
   {
-    type: 'test-ipc', // 调用时需加上`@${id}`，此处为 'test-ipc@translime-plugin-my-plugin'
-    handler: ({ sendToClient }) => (arg1, arg2) => {
-      console.log('test-ipc', 'test ipc from plugin: ', arg1, arg2);
-      sendToClient(`test-ipc-reply@${id}`, 'test ipc reply from plugin');
+    type: 'record', // 调用时需加上`@${id}`，此处为 'test-ipc@translime-plugin-my-plugin'
+    handler: () => (url) => {
+      const outputFilePath = 'C:\\Users\\admin\\AppData\\Roaming\\translime\\recorder-test\\test.mp4';
+      const durationInSeconds = 60;
+      let fileCounter = 1;
+
+      const startRecord = () => {
+        const setting = global.store.get(`plugin.${id}.settings`, {});
+        const command = ffmpeg(url);
+        setFfmpegPath(command, setting['ffmpeg-path']);
+        command.output(outputFilePath)
+          .on('end', () => {
+            console.log('on end event');
+          })
+          .on('progress', (progress) => {
+            currentProgress = progress;
+            console.log(`Processing: ${progress.percent}% done`);
+          })
+          .run();
+      };
+
+      startRecord();
+    },
+  },
+  {
+    type: 'progress',
+    handler: ({ sendToClient }) => () => {
+      sendToClient(`progress-reply@${id}`, currentProgress);
     },
   },
 ];
@@ -95,6 +102,7 @@ const ipcHandlers = [
 module.exports = {
   pluginDidLoad,
   pluginWillUnload,
+  pluginSettingSaved,
   settingMenu,
   pluginMenu,
   ipcHandlers,
--- src/ui.vue
+++ src/ui.vue
@@ -1,4 +1,24 @@
-<script setup></script>
+<script setup>
+import { ref, onMounted } from 'vue';
+
+const ipc = window.electron.useIpc();
+const pluginId = 'translime-plugin-rtmp-recorder';
+
+const url = ref('');
+const progress = ref({});
+const start = () => {
+  ipc.send(`record@${pluginId}`, url.value);
+};
+const onProgress = () => {
+  ipc.on(`progress@${pluginId}`, (p) => {
+    progress.value = p;
+  });
+};
+
+onMounted(() => {
+  onProgress();
+});
+</script>
 
 <script>
 const vuetify = window.vuetify$.components;
@@ -13,11 +33,27 @@ export default {
 </script>
 
 <template>
-  <v-container fluid class="plugin-main">
-    <div>hello translime</div>
+  <v-container class="plugin-main">
+    <v-card rounded="xl" variant="tonal">
+      <v-card-text>
+        <div>
+          <v-text-field v-model="url" color="primary" label="直播源" hint="设置直播来源" />
+        </div>
 
-    <div>
-      <v-btn>vuetify button</v-btn>
-    </div>
+        <div class="mt-4">
+          <v-btn>更多设置</v-btn>
+        </div>
+
+        <div class="d-flex mt-4">
+          <v-spacer />
+
+          <v-btn color="primary" rounded="pill" @click="start">开始录制</v-btn>
+        </div>
+
+        <div class="mt-4">
+          <pre v-text="JSON.stringify(progress, null, 2)"></pre>
+        </div>
+      </v-card-text>
+    </v-card>
   </v-container>
 </template>
